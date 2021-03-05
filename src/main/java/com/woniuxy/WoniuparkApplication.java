1be8aa8503d7832f8b5cac075be7506bb7dd3a9a
who-github <544576879@qq.com>
2021-03-05 14:48:14 +0800

    woniupark的原始项目模型（第一次提交）

---
+++ src/main/java/com/woniuxy/WoniuparkApplication.java
@@ -0,0 +1,13 @@
+package com.woniuxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoniuparkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoniuparkApplication.class, args);
+    }
+
+}
